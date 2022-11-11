55e20e27a7b0d769b0b16ac6869629dfa5cc6ccc
simonavsi03 <115585075+simonavsi03@users.noreply.github.com>
2022-11-10 20:00:23 -0600

    Ultimo quiz de corte 3

---
+++ QuizUltimo.py
@@ -0,0 +1,15 @@
+rios = { "Nilo": "Egipto",
+         "Amazonas": "Colombia",
+         "Sumida": "Japon",
+         "Yangtze": "China",
+         "Han": "Corea del sur"}
+
+for i,j in rios.items():
+    print("El " + i  + " Corre a travez de: " + j )
+for i,j in rios.items():
+    print(i)
+for i,j in rios.items():
+    print(j)
+
+
+
---
+++ quiz, punto 2.py
@@ -0,0 +1,9 @@
+palabras = {"diccionario": "método de arreglo en python",
+            "INT": "variable que es un numero entero",
+            "IDE": "aplicacion que sirve para ejecutar programas de codigo",
+            "GIT HUB": "aplicacion que sirve para subir el codigo a internet",
+            "String": "variable de tipo letra o palabra", }
+
+for i,j in palabras.items():
+    print(i + " Tiene como significado: " + j)
+
